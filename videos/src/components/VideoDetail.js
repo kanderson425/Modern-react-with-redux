7db719cba384bc0ebde71670957bb1fda66d8cfd
kanderson425 <kyleanderson.dev@gmail.com>
2021-06-06 15:44:30 -0600

    mAdds the working VideoDetail component.

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -5,7 +5,27 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>;
   }
 
-  return <div>{video.snippet.title}</div>;
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+  return (
+    <div>
+      <div className="ui embbed">
+        <iframe
+          width="560"
+          height="315"
+          src={videoSrc}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
