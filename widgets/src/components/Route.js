0abc34815542d466e382ee55fd8b5b85d75b3eea
kanderson425 <kyleanderson.dev@gmail.com>
2021-06-13 16:49:01 -0600

    Adds manually built Navigation component.

---
+++ widgets/src/components/Route.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const Route = ({ path, children }) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  const onLocationChange = () => {
+    setCurrentPath(window.location.pathname);
+  };
+
+  useEffect(() => {
+    window.addEventListener("popstate", onLocationChange);
+
+    return () => {
+      window.removeEventListener("popstate", onLocationChange);
+    };
+  }, []);
+
+  return currentPath === path ? children : null;
+};
+
+export default Route;
