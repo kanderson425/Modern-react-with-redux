42c7eac08e886065f43b4a614cf1caa3396f8502
kanderson425 <kyleanderson.dev@gmail.com>
2021-06-17 10:10:44 -0500

    Adds the completed sonog selection app integrated with simple redux.

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const SongDetail = () => {
-  return <div></div>;
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a Song</div>;
+  }
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: {song.title}
+        <br />
+        Duration: {song.duration}
+      </p>
+    </div>
+  );
 };
 
-export default SongDetail;
+const mapStateToProps = (state) => {
+  return {
+    song: state.selectedSong,
+  };
+};
+
+export default connect(mapStateToProps)(SongDetail);
--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -5,7 +5,7 @@ export const songsReducer = () => {
     { title: "No Scrubs", duration: "4:05" },
     { title: "Macarena", duration: "2:30" },
     { title: "All Star", duration: "3:15" },
-    { title: "I want it that way", duuration: "1:45" },
+    { title: "I want it that way", duration: "1:45" },
   ];
 };
 
