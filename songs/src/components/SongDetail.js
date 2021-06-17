7efa43035ff2217c7930840c0cb52020f37ccabb
kanderson425 <kyleanderson.dev@gmail.com>
2021-06-16 22:58:28 -0500

    Adds the selectSong action creator to the actions/index.js file.

---
+++ songs/src/components/SongDetail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SongDetail = () => {
+  return <div></div>;
+};
+
+export default SongDetail;
---
+++ songs/src/components/SongList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SongList = () => {
+  return <div></div>;
+};
+
+export default SongList;
