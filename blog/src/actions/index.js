170863387cb2fb6d60493d1b4edb6d0dc8ced4fc
kanderson425 <kyleanderson.dev@gmail.com>
2021-06-21 17:35:50 -0500

    Adds the completed posts reducer and completes the PostList component.

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,7 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
+export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
 
-  return {
-    type: "FETCH_POSTS",
-    payload: response,
-  };
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
