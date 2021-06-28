6914c100ae40315b316af9d2502ec9054484b6d4
kanderson425 <kyleanderson.dev@gmail.com>
2021-06-28 09:55:09 -0500

    Adds the deleteStream action creator.

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -46,3 +46,9 @@ export const editStream = (id, formValues) => async (dispatch) => {
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
 };
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
