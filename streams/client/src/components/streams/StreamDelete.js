5348b0905592b8b1ad0823aba1adae40055d2081
kanderson425 <kyleanderson.dev@gmail.com>
2021-07-06 21:54:23 -0500

    Adds the fetchStream action creator and wires up the dynamic title text display in the Delete Modal.

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,13 +1,47 @@
 import React from "react";
+import { connect } from "react-redux";
 import Modal from "../Modal";
+import history from "../../history";
+import { deleteStream, fetchStream } from "../../actions";
 
-const StreamDelete = () => {
-  return (
-    <div>
-      StreamDelete
-      <Modal />
-    </div>
-  );
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete this stream?";
+    }
+    return `Are you sure you want to delete the stream with title: ${this.props.stream.title}`;
+  }
+
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <Modal
+        title="Delete Stream"
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={() => history.push("/")}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, {
+  fetchStream,
+  deleteStream,
+})(StreamDelete);
